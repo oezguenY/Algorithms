aedc8e69e4e85bb7f70af014caf1f5ca18d0a122
Ozgun Yildiz <ozgun.yildiz@hotmail.de>
2022-04-14 13:28:15 +0200

    Added QuickSort

---
+++ Algorithms.playground/Pages/Quick Sort.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+func partitionLomuto2(_ a: inout [Int], low: Int, high: Int) -> Int {
+    let pivot = a[high]
+    var i = low
+    for j in low..<high {
+        if a[j] <= pivot {
+            (a[i], a[j]) = (a[j], a[i])
+            i += 1
+        }
+    }
+    (a[i], a[high]) = (a[high], a[i])
+    return i
+}
+
+func partitionLomuto<T: Comparable>(_ a: inout [T], low: Int, high: Int) -> Int {
+  let pivot = a[high]
+
+  var i = low
+  for j in low..<high {
+    if a[j] <= pivot {
+      (a[i], a[j]) = (a[j], a[i])
+      i += 1
+    }
+  }
+
+  (a[i], a[high]) = (a[high], a[i])
+  return i
+}
+
+func quicksortLomuto<T: Comparable>(_ a: inout [T], low: Int, high: Int) {
+  if low < high {
+    let p = partitionLomuto(&a, low: low, high: high)
+    quicksortLomuto(&a, low: low, high: p - 1)
+    quicksortLomuto(&a, low: p + 1, high: high)
+  }
+}
+
+var list = [4,6,9,1,2,5]
+quicksortLomuto(&list, low: 0, high: list.count - 1)
+
